d360e6bccfc87c11a0197e29102320cc209930f8
Jonathan Ayala <yojona@msn.com>
2023-08-06 13:28:41 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from './components/Home';
+import {
+  Routes,
+  Route,
+  HashRouter,
+} from "react-router-dom";
+import Privacy from './components/Privacy';
 
+const styles = {
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column'
+  },
+  logo: {
+    marginTop: 32
+  }
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={styles.container}>
+      <HashRouter>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/privacy" element={<Privacy />} />
+        </Routes>
+      </HashRouter>
     </div>
+
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Logo from './Logo';
+import Games from './Games';
+
+const Home = () => {
+  return (
+    <>
+      <Logo />
+      <Games />
+    </>
+  );
+}
+
+export default Home;
